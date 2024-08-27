4d8314070ac0931a97a1e5c924cb58607fa8ff37
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-08-27 13:06:11 +0400

    Add CRUD methods to work with db

--- src/db.js
+++ src/db.js
@@ -12,7 +12,7 @@ export const saveDB = async (db) => {
   return db;
 };
 
-export const insert = async (data) => {
+export const insertDB = async (data) => {
   const db = await getDB();
   db.notes.push(data);
   await saveDB(db);
